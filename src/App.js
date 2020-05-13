994c13c2ece7513d19dc9e7028a7b3384341d5b5
saketh mandapaka <satyasaketh999@gmail.com>
2020-05-13 18:33:21 +0530

    sampleDataAdded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import data from './sampledata.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+	};    
+
+  }
+ 
+  
+  goToNext = () => {
+    this.setState({ index: (this.state.index + 1) % data.length });
+  };
+  
+  render() {
+    const item = data[this.state.index];
+    return (<div><h3>Question.{this.state.index+1}:{item.question}</h3>
+		<p>{item.op1}</p>
+		<p>{item.op2}</p>
+		<p>{item.op3}</p>
+		<p>ANS: {item.Ans}</p>
+		<button onClick={this.goToNext}>next</button></div>);
+  }
 }
-
 export default App;
+
+
+
+
+
+
+
+
