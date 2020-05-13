de20bffa1cf038fa5bc9badbd928ffd80a6f01eb
pvdinesh <pvdinesh1309@gmail.com>
2020-05-13 20:19:19 +0530

    added firebase credenitals

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDRFFNkBxWHX7Gzy3Jag04D0Wnb2OPwyG8",
+  authDomain: "quiz-b60f8.firebaseapp.com",
+  databaseURL: "https://quiz-b60f8.firebaseio.com",
+  projectId: "quiz-b60f8",
+  storageBucket: "quiz-b60f8.appspot.com",
+  messagingSenderId: "10406340831",
+  appId: "1:10406340831:web:66fff03848571381a1216a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
