4b7ae2852fd97b88ddcdbb108e91dad9407420dc
pvdinesh <pvdinesh1309@gmail.com>
2020-05-13 23:59:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
 import React,{Component} from 'react';
 import './App.css';
-import data from './sampledata.json'
+import { db } from './services/firebase'
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      index: 0
-	};    
-
+      questions: ""
+    }
   }
+  state = {
+     
+   }
  
-  
-  goToNext = () => {
-    this.setState({ index: (this.state.index + 1) % data.length });
-  };
+  componentDidMount(){
+    console.log("mounted")
+    db.collection('questions')
+      .get()
+      .then( snapshot => {
+        console.log(snapshot)
+        const questions = []
+        snapshot.forEach( doc => {
+          doc = doc.data()
+          questions.push(doc)
+        })
+        this.setState({questions: questions})
+      })
+      .catch( error => console.log(error))
+  }
+
+
   
   render() {
-    const item = data[this.state.index];
-    return (<div><h3>Question.{this.state.index+1}:{item.question}</h3>
-		<p>{item.op1}</p>
-		<p>{item.op2}</p>
-		<p>{item.op3}</p>
-		<p>ANS: {item.Ans}</p>
-		<button onClick={this.goToNext}>next</button></div>);
+    
+    return (
+      <div className="questions">
+        <div>Quiz</div> 
+        {
+          this.state.questions &&
+          this.state.questions.map( question => {
+            return (
+            <div>
+              <p>Question: {question.question}</p>
+              <p>1. {question.op1}</p>
+              <p>2. {question.op2}</p>
+              <p>3. {question.op3}</p>
+              <p>4. {question.op4}</p>
+              <p>Answer: {question.answer}</p>
+            </div>
+            )
+          })
+        }
+      </div>
+    )
+    
   }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
 
-
-var firebaseConfig = {
-  apiKey: "AIzaSyDRFFNkBxWHX7Gzy3Jag04D0Wnb2OPwyG8",
-  authDomain: "quiz-b60f8.firebaseapp.com",
-  databaseURL: "https://quiz-b60f8.firebaseio.com",
-  projectId: "quiz-b60f8",
-  storageBucket: "quiz-b60f8.appspot.com",
-  messagingSenderId: "10406340831",
-  appId: "1:10406340831:web:66fff03848571381a1216a"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -29,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
